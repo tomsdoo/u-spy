68f21bfda8d9c1e3e3c412387f5f299745e5b9ad
tomsdoo <toms@live.jp>
2025-04-24 21:28:30 +0900

    beacon

--- src/components/log/list/template.ts
+++ src/components/log/list/template.ts
@@ -2,6 +2,7 @@ import { ControlElement, ControlEvents } from "@/components/control-element";
 
 type FetchLog = ControlElement["logStore"][ControlEvents.FETCH] extends (infer T)[] ? T : never;
 type XhrLog = ControlElement["logStore"][ControlEvents.XHR_LOAD] extends (infer T)[] ? T : never;
+type BeaconLog = ControlElement["logStore"][ControlEvents.BEACON] extends (infer T)[] ? T : never;
 
 const formatter = new Intl.DateTimeFormat("en-US", {
   hour12: false,
@@ -34,6 +35,9 @@ export function template(id: string, logItems: ControlElement["logItems"]) {
   function validateXhrLog(logItem: typeof logItems extends (infer T)[] ? T : never): logItem is XhrLog & { type: "xhr" } {
     return logItem.type === "xhr";
   }
+  function validateBeaconLog(logItem: typeof logItems extends (infer T)[] ? T : never): logItem is BeaconLog & { type: "beacon" } {
+    return logItem.type === "beacon";
+  }
   return `
     <div id="${id}">
       <form>
@@ -44,9 +48,11 @@ export function template(id: string, logItems: ControlElement["logItems"]) {
           logItems.map(logItem => {
             const isFetchLog = validateFetchLog(logItem);
             const isXhrLog = validateXhrLog(logItem);
+            const isBeaconLog = validateBeaconLog(logItem);
             const liClassNames = [
               isFetchLog ? "fetch-log" : "",
               isXhrLog ? "xhr-log" : "",
+              isBeaconLog ? "beacon-log" : "",
             ].join(" ");
             const fetchLogHtml = isFetchLog
               ? `
@@ -82,12 +88,26 @@ export function template(id: string, logItems: ControlElement["logItems"]) {
                 </div>
               `
               : "";
+            const beaconLogHtml = isBeaconLog
+              ? `
+                <div class="method"></div>
+                <div class="host">
+                  <abbr title="${logItem.data.url}">${getHost(logItem.data.url)}</abbr>
+                  <a href="${logItem.data.url}" target="_blank">${logItem.data.url}</a>
+                </div>
+                <div data-foldable class="body folded">
+                  ${logItem.data.data ?? ""}
+                </div>
+                <div class="response"></div>
+              `
+              : "";
             return `
             <li id="${logItem.id}" class="${liClassNames}">
               <div class="time">${formatTime(logItem.time)}</div>
               <div class="type">${logItem.type}</div>
               ${ fetchLogHtml }
-               ${ xhrLogHtml }
+              ${ xhrLogHtml }
+              ${ beaconLogHtml }
             </li>
             `;
           }).join("")
@@ -186,6 +206,59 @@ export function template(id: string, logItems: ControlElement["logItems"]) {
         }
 
         > li.xhr-log {
+           display: grid;
+           grid-template:
+             "time type method host" auto
+             "body body body body" auto
+             "response response response response" auto / auto auto auto 1fr;
+          gap: 0.2em 0.6em;
+
+          > div {
+           word-break: break-all;
+           line-height: 1.4;
+          }
+
+          > .time {
+            grid-area: time;
+          }
+          > .type {
+            grid-area: type;
+            text-transform: uppercase;
+            color: darkgoldenrod;
+          }
+          > .method {
+            grid-area: method;
+            color: darkkhaki;
+          }
+          > .host {
+            grid-area: host;
+            > a {
+              display: none;
+            }
+          }
+          > .host.detailed {
+            > abbr {
+              display: none;
+            }
+            > a {
+              display: inline;
+              color: darkcyan;
+            }
+          }
+          > .body {
+            grid-area: body;
+          }
+          > .response {
+            grid-area: response;
+          }
+          > .body.folded,
+          > .response.folded {
+            overflow: hidden;
+            max-height: calc(1.5em * 2);
+          }
+        }
+
+        > li.beacon-log {
            display: grid;
            grid-template:
              "time type method host" auto
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { ControlElement } from "@/components/control-element";
 import { appendEntryPoint } from "@/components/entry-point";
 import { interceptXMLHttpRequest, type MockXHRHandler } from "@/xml-http-request";
 import { interceptFetch, type MockFetchHandler } from "@/fetch";
+import { interceptSendBeacon } from "@/beacon";
 import { displayDialog } from "@/components/dialog";
 import { registerHotStroke } from "@/key-event";
 
@@ -25,15 +26,18 @@ globalThis._spy = {
     const receiver = ControlElement.ensure(id);
     const { restoreXMLHttpRequest } = interceptXMLHttpRequest(id, options?.xhrHandlers);
     const { restoreFetch } = interceptFetch(id, options?.fetchHandlers);
+    const { restoreSendBeacon } = interceptSendBeacon(id);
     function restore() {
       restoreXMLHttpRequest();
       restoreFetch();
+      restoreSendBeacon();
     }
     return {
       receiver,
       restore,
       restoreXMLHttpRequest,
       restoreFetch,
+      restoreSendBeacon,
     };
   },
   unregisterHotStroke,
