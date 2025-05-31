fe53d9f47ecdc79d5fbcd1226f9faf2651ca6380
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2025-05-31 10:47:41 +0900

    change hot stroke (#48)

--- src/index.ts
+++ src/index.ts
@@ -21,17 +21,21 @@ EntryPointElement.ensure();
 StoreElement.ensure();
 MessageBusElement.ensure();
 
+const unregisterHotStrokeMap = new Map<string, () => void>();
+
 const {
   unregisterHotStroke: unregisterHotStrokeSpy,
 } = registerHotStroke("spy", () => {
   displayDialog("spy");
 });
+unregisterHotStrokeMap.set("spy", unregisterHotStrokeSpy);
 
 const {
   unregisterHotStroke: unregisterHotStrokeStyle,
 } = registerHotStroke("style", () => {
   displayDialog("style");
 });
+unregisterHotStrokeMap.set("style", unregisterHotStrokeStyle);
 
 globalThis._spy = {
   intercept(id: string, options?: InterceptionOptions) {
@@ -54,8 +58,33 @@ globalThis._spy = {
   },
   registerHotStroke,
   displayDialog,
+  changeHotStrokeSpy(nextStroke: string) {
+    const defaultUnregisterHotStroke = unregisterHotStrokeMap.get("spy");
+    if (defaultUnregisterHotStroke == null) {
+      return;
+    }
+    defaultUnregisterHotStroke();
+    const hotStroke = registerHotStroke(nextStroke, () => {
+      displayDialog("spy");
+    });
+    unregisterHotStrokeMap.set(nextStroke, hotStroke.unregisterHotStroke);
+    return hotStroke;
+  },
+  changeHotStrokeStyle(nextStroke: string) {
+    const defaultUnregisterHotStroke = unregisterHotStrokeMap.get("style");
+    if (defaultUnregisterHotStroke == null) {
+      return;
+    }
+    defaultUnregisterHotStroke();
+    const hotStroke = registerHotStroke(nextStroke, () => {
+      displayDialog("style");
+    });
+    unregisterHotStrokeMap.set(nextStroke, hotStroke.unregisterHotStroke);
+    return hotStroke;
+  },
   unregisterHotStroke() {
-    unregisterHotStrokeSpy();
-    unregisterHotStrokeStyle();
+    for(const unregisterAHotStroke of unregisterHotStrokeMap.values()) {
+      unregisterAHotStroke();
+    }
   },
 };
