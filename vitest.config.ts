bc6e6f46fc85b284ec4dee16bf82561a2aabe584
tomsdoo <toms@live.jp>
2025-07-05 06:21:50 +0900

    vitest/browser

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,43 @@ import { fileURLToPath } from "url";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+  test: {
+    projects: [
+      {
+        test: {
+          include: ["**/*.browser.test.{ts,js}"],
+          name: "browser",
+          browser: {
+            enabled: true,
+            provider: "playwright",
+            headless: true,
+            instances: [
+              {
+                browser: "chromium",
+              },
+            ],
+          },
+        },
+        resolve: {
+          alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+            "@@": fileURLToPath(new URL("./", import.meta.url)),
+          },
+        },
+      },
+      {
+        test: {
+          include: ["**/*.node.test.{ts,js}"],
+          name: "node",
+          environment: "node",
+        },
+        resolve: {
+          alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+            "@@": fileURLToPath(new URL("./", import.meta.url)),
+          },
+        },
+      },
+    ],
   },
 });
