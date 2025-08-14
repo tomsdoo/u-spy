723e38ddf33c311a298ebdc73b8490da58bb2f13
tomsdoo <toms@live.jp>
2025-08-15 06:07:34 +0900

    test for beacon

---
+++ src/beacon.browser.test.ts
@@ -0,0 +1,47 @@
+import { beforeEach, afterEach, describe, it, expect, vi } from "vitest";
+import { interceptSendBeacon } from "@/beacon";
+import { ControlElement } from "@/components/control-element";
+
+const {
+  spySendBeacon,
+  controlId,
+  dummyUrl,
+  dummyData,
+} = vi.hoisted(() => ({
+  spySendBeacon: vi.fn(),
+  controlId: "control-id",
+  dummyUrl: "http://dummy.com",
+  dummyData: "dummyData",
+}));
+
+describe("interceptSendBeacon", () => {
+  beforeEach(() => {
+    vi.spyOn(globalThis.navigator, "sendBeacon")
+      .mockImplementation(spySendBeacon);
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("intercept and restore", () => {
+    const spyEnsure = vi.spyOn(ControlElement, "ensure");
+    const {
+      restoreSendBeacon,
+    } = interceptSendBeacon(controlId);
+
+    navigator.sendBeacon(dummyUrl, dummyData);
+    expect(spyEnsure).toHaveBeenCalledTimes(1);
+    expect(spyEnsure).toHaveBeenCalledWith(controlId);
+    expect(spySendBeacon).toHaveBeenCalledTimes(1);
+    expect(spySendBeacon).toHaveBeenCalledWith(dummyUrl, dummyData);
+
+    restoreSendBeacon();
+
+    navigator.sendBeacon(dummyUrl, dummyData);
+    expect(spyEnsure).toHaveBeenCalledTimes(1);
+    expect(spySendBeacon).toHaveBeenCalledTimes(2);
+    expect(spySendBeacon).toHaveBeenCalledWith(dummyUrl, dummyData);
+  });
+});
+
