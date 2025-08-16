73c3840dd7db970a3eff5fbc7121a749d2d8efc6
tomsdoo <toms@live.jp>
2025-08-16 11:23:51 +0900

    test for entry-point

---
+++ src/components/entry-point/index.browser.test.ts
@@ -0,0 +1,28 @@
+import { beforeEach, describe, it, expect, vi } from "vitest";
+import { EntryPointElement } from "@/components/entry-point";
+
+const {
+  dummyName,
+  dummyVersion,
+} = vi.hoisted(() => ({
+  dummyName: "dummyName",
+  dummyVersion: "dummyVersion",
+}));
+
+vi.mock("@@/package.json", () => ({
+  name: dummyName,
+  version: dummyVersion,
+}));
+
+describe("EntryPointElement", () => {
+  beforeEach(() => {
+    document.body.innerHTML ="";
+  });
+  it("renders correctly", () => {
+    const entryPointElement = document.body.appendChild(document.createElement(EntryPointElement.TAG_NAME));
+    expect(entryPointElement.shadowRoot).toSatisfy(({ innerHTML }) => {
+      const matched = innerHTML.match(/dummyName@dummyVersion/);
+      return matched != null;
+    });
+  });
+});
