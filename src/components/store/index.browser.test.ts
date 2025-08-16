9aba7562420b64da7e6f7f3df19a699eb5fbe3ef
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2025-08-16 11:22:35 +0900

    test for store (#106)

---
+++ src/components/store/index.browser.test.ts
@@ -0,0 +1,54 @@
+import { beforeEach, afterEach, describe, it, expect, vi, type MockInstance } from "vitest";
+import { StoreElement, ensureStore, getStoreIds } from "@/components/store/";
+
+describe("store", () => {
+  beforeEach(() => {
+    document.body.innerHTML = "";
+  });
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+  describe("getStoreIds", () => {
+    let spy: MockInstance;
+    beforeEach(() => {
+      spy = vi.spyOn(StoreElement.prototype, "freeData", "get")
+        .mockReturnValue(new Map([
+          [
+            "dummyKey1",
+            {
+              _onChangeHandlers: [],
+              onChange() {},
+              offChange() {},
+            },
+          ],
+          [
+            "dummyKey2",
+            {
+              _onChangeHandlers: [],
+              onChange() {},
+              offChange() {},
+            },
+          ],
+        ]));
+    });
+    afterEach(() => {
+      vi.restoreAllMocks();
+    });
+    it("returns correct value", () => {
+      expect(getStoreIds()).toEqual(["dummyKey1","dummyKey2"]);
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it("ensureStore", () => {
+    const spyStoreEnsure = vi.spyOn(StoreElement, "ensure");
+    expect(getStoreIds()).toHaveLength(0);
+    expect(spyStoreEnsure).toHaveBeenCalledTimes(1);
+    ensureStore("dummyId1");
+    expect(spyStoreEnsure).toHaveBeenCalledTimes(2);
+    expect(getStoreIds()).toHaveLength(1);
+    expect(spyStoreEnsure).toHaveBeenCalledTimes(3);
+  });
+});
+
+
