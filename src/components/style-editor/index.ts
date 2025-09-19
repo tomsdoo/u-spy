6b59ff61c623f718a5e003bf63fc615a51b1cab0
tomsdoo <toms@live.jp>
2025-09-19 19:33:06 +0900

    style-editor save/load

--- src/components/style-editor/index.ts
+++ src/components/style-editor/index.ts
@@ -1,10 +1,14 @@
 import { BaseElement } from "@/components/base";
+import { InputFormElement } from "@/components/input-form";
+import { SelectFormElement } from "@/components/select-form";
 import { UtilsElement } from "@/components/utils";
 import { EventType } from "@/constants/event-type";
+import { createStorageProxy } from "@/storage";
 import { template } from "./template";
 
 const TAG_NAME = "u-spy-style-editor";
 const STYLE_TAG_ID = "u-spy-style-edited";
+const STORAGE_PREFIX = "_spy_style_";
 
 function ensureStyleTag() {
   const existingStyleTag = document.querySelector<HTMLStyleElement>(
@@ -23,6 +27,7 @@ export class StyleEditorElement extends BaseElement {
   id: string;
   _styleText: string;
   _styleTag: HTMLStyleElement;
+  _storage: ReturnType<typeof createStorageProxy>;
   static get observedAttributes() {
     return [];
   }
@@ -40,20 +45,29 @@ export class StyleEditorElement extends BaseElement {
     this.id = `usse-${crypto.randomUUID()}`;
     this._styleTag = ensureStyleTag();
     this._styleText = this.styleTag.innerHTML;
+    this._storage = createStorageProxy(STORAGE_PREFIX);
   }
   get styleTag() {
     return this._styleTag ?? ensureStyleTag();
+  }
+  get storageData() {
+    try {
+      return JSON.parse(this._storage.data);
+    } catch {
+      return {};
+    }
   }
   connectedCallback() {
     this._styleText = this.styleTag.innerHTML;
     this.render();
   }
   onRendered() {
+    const that = this;
     this.addEventListener(EventType.CLICK, (e) => {
       e.stopPropagation();
     });
     const textarea = this.querySelector<HTMLTextAreaElement>(
-      `#${this.id} > form > textarea`,
+      `#${this.id} > .editor-form > textarea`,
     );
     if (textarea == null) {
       return;
@@ -104,6 +118,123 @@ export class StyleEditorElement extends BaseElement {
         "css",
       );
       textarea.value = this.styleText;
+    });
+    function getElement<T extends Element>(selector: string) {
+      const element = that.querySelector<T>(selector);
+      return {
+        error: element == null,
+        element,
+      };
+    }
+    const {
+      error,
+      result: {
+        saveButton,
+        saveForm,
+        loadButton,
+        selectForm,
+      },
+    } = [
+      {
+        name: "saveButton",
+        selector: `#${this.id} .save-button`,
+      },
+      {
+        name: "saveForm",
+        selector: `#${this.id} .save-form`,
+      },
+      {
+        name: "loadButton",
+        selector: `#${this.id} .load-button`,
+      },
+      {
+        name: "selectForm",
+        selector: `#${this.id} .select-form`,
+      },
+    ].reduce((acc, {name, selector}) => {
+      const { error, element } = getElement(selector);
+      return {
+        ...acc,
+        error: acc.error || error,
+        result: {
+          ...acc.result,
+          [name]: element,
+        },
+      };
+    }, { error: false, result: {} as Record<string, Element | null>}) as unknown as {
+      error: boolean;
+      result: {
+        saveButton: HTMLButtonElement;
+        saveForm: HTMLFormElement;
+        loadButton: HTMLButtonElement;
+        selectForm: HTMLFormElement;
+      };
+    };
+    if (error) {
+      return;
+    }
+    saveButton.addEventListener(EventType.CLICK, () => {
+      function saveHandler(e: {detail: { value: string }}) {
+        const nextData = {
+          ...that.storageData,
+          // biome-ignore lint/style/noNonNullAssertion: textarea exists
+          [e.detail.value]: textarea!.value,
+        };
+        that._storage.data = JSON.stringify(nextData);
+        saveForm.removeEventListener(InputFormElement.FINISH_INPUT_EVENT, saveHandler);
+        saveForm.removeEventListener(
+          InputFormElement.CANCEL_EVENT,
+          cancelHandler,
+        );
+        saveForm?.classList.add("hidden");
+      }
+      function cancelHandler() {
+        saveForm.removeEventListener(
+          InputFormElement.FINISH_INPUT_EVENT,
+          saveHandler,
+        );
+        saveForm.removeEventListener(
+          InputFormElement.CANCEL_EVENT,
+          cancelHandler,
+        );
+        saveForm?.classList.add("hidden");
+      }
+      // @ts-expect-error handler type will be resolved
+      saveForm.addEventListener(InputFormElement.FINISH_INPUT_EVENT, saveHandler);
+      saveForm.addEventListener(InputFormElement.CANCEL_EVENT, cancelHandler);
+      saveForm.classList.remove("hidden");
+    });
+    loadButton.addEventListener(EventType.CLICK, () => {
+      const options = Array.from(Object.keys(that.storageData));
+      function chooseHandler(e: {detail: { value: string }}) {
+        textarea.value = that.storageData[e.detail.value];
+        that.styleText = that.storageData[e.detail.value];
+        selectForm.classList.add("hidden");
+        selectForm.removeEventListener(SelectFormElement.CHOOSE_EVENT, chooseHandler);
+        selectForm.removeEventListener(SelectFormElement.REMOVE_EVENT, removeHandler);
+        selectForm.removeEventListener(SelectFormElement.CANCEL_EVENT, cancelHandler);
+      }
+      function removeHandler(e: { detail: { value: string }}) {
+        const nextData = Object.fromEntries(
+          Object.entries(that.storageData).filter(([key]) => key !== e.detail.value)
+        );
+        that._storage.data = JSON.stringify(nextData);
+        selectForm.classList.add("hidden");
+        selectForm.removeEventListener(SelectFormElement.CHOOSE_EVENT, chooseHandler);
+        selectForm.removeEventListener(SelectFormElement.REMOVE_EVENT, removeHandler);
+        selectForm.removeEventListener(SelectFormElement.CANCEL_EVENT, cancelHandler);
+      }
+      function cancelHandler(e: { detail: { value: string }}) {
+        selectForm.classList.add("hidden");
+        selectForm.removeEventListener(SelectFormElement.CHOOSE_EVENT, chooseHandler);
+        selectForm.removeEventListener(SelectFormElement.REMOVE_EVENT, removeHandler);
+        selectForm.removeEventListener(SelectFormElement.CANCEL_EVENT, cancelHandler);
+      }
+      selectForm.setAttribute(":options", options.join(","));
+      selectForm.addEventListener(SelectFormElement.CHOOSE_EVENT, chooseHandler);
+      selectForm.addEventListener(SelectFormElement.REMOVE_EVENT, removeHandler);
+      selectForm.addEventListener(SelectFormElement.CANCEL_EVENT,  cancelHandler);
+      selectForm.classList.remove("hidden");
     });
   }
   onStyleTextChange() {
