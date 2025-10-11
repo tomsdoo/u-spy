4a3f3b892260bdd10ca912c034c161dcc9d88393
tomsdoo <toms@live.jp>
2025-10-11 15:38:23 +0900

    add some lines into scaffolding code

--- scaffold/chrome-extension/src/index.ts
+++ scaffold/chrome-extension/src/index.ts
@@ -3,3 +3,11 @@ import "u-spy";
 _spy.stroke.register("hello", () => {
   _spy.showEphemeralMessage("world");
 });
+
+_spy.stroke.register("dialog", () => {
+  _spy.dialog.display((dialogElement) => {
+    const div = document.createElement("div");
+    div.textContent = "hello from dialog";
+    dialogElement.appendChild(div);
+  });
+});
