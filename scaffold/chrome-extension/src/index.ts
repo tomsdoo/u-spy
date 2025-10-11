48a076b872c6f84a3846240e7e60f68a5aad39e2
tomsdoo <toms@live.jp>
2025-10-11 15:43:34 +0900

    interception code

--- scaffold/chrome-extension/src/index.ts
+++ scaffold/chrome-extension/src/index.ts
@@ -1,13 +1,47 @@
 import "u-spy";
 
-_spy.stroke.register("hello", () => {
-  _spy.showEphemeralMessage("world");
+const {
+  receiver,
+  restore,
+} = _spy.intercept("my-extension");
+
+receiver.on(
+  receiver.events.FETCH,
+  (data) => {
+    console.log("fetch", data);
+  },
+);
+
+receiver.on(
+  receiver.events.XHR_LOAD,
+  (data) => {
+    console.log("xhr load", data);
+  },
+);
+
+receiver.on(
+  receiver.events.BEACON,
+  (data) => {
+    console.log("beacon", data);
+  },
+);
+
+receiver.on(
+  receiver.events.WINDOW_MESSAGE,
+  (data) => {
+    console.log("window message", data);
+  },
+);
+
+_spy.stroke.register("restore", () => {
+  restore();
+  _spy.showEphemeralMessage("restored interceptions");
 });
 
-_spy.stroke.register("dialog", () => {
+_spy.stroke.register("hello", () => {
   _spy.dialog.display((dialogElement) => {
     const div = document.createElement("div");
-    div.textContent = "hello from dialog";
+    div.textContent = "world";
     dialogElement.appendChild(div);
   });
 });
