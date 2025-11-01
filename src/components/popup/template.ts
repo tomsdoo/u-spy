89b6225755834ad34fd7a8fa0b433a0cce340843
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2025-11-01 10:41:24 +0900

    ephemeral message will be copyable (#183)

--- src/components/popup/template.ts
+++ src/components/popup/template.ts
@@ -8,6 +8,10 @@ export function template(id: string) {
       top: 10vh;
       left: 50%;
       transform: translate(-50%, 0);
+      margin-block: 0;
+      padding-inline: 0;
+      list-style: none;
+
       li {
         background: rgb(0 0 0 / 80%);
         color: #ffffff;
@@ -15,11 +19,35 @@ export function template(id: string) {
         padding: 32px;
         border-radius: 8px;
         opacity: 0;
+        list-style: none;
+        cursor: pointer;
+
+        pre {
+          word-break: break-all;
+          white-space: pre-wrap;
+          margin-block: 0;
+        }
 
         &.visible {
           opacity: 1;
         }
         transition: opacity 1s;
+
+        &.copied {
+          position: relative;
+          cursor: default;
+
+          &::before {
+            position: absolute;
+            content: "copied";
+            top: 0;
+            right: 0;
+            display: block;
+            padding: 1em;
+            font-size: 0.8em;
+            color: #ffff88;
+          }
+        }
       }
     }
     </style>
