fae6976543870b096d33e4c71ee8392feb158bc0
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2025-11-18 12:50:29 +0900

    formatTime formatType is added (#191)

--- src/utils/format-time.ts
+++ src/utils/format-time.ts
@@ -7,19 +7,33 @@ const logTimeFormatterHhMmSsFff = new Intl.DateTimeFormat("en-US", {
   timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
 });
 
+const logTimeFormatterYyyyMMddHhMmSsFff = new Intl.DateTimeFormat("ja-JP", {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+  hour12: false,
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  fractionalSecondDigits: 3,
+  timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+});
+
 export enum FormatType {
-  HH_MM_SS_FFF = "hh:mm:ss.fff",
+  HH_MM_SS_FFF = "HH:mm:ss.fff",
+  YYYY_MM_DD_HH_MM_SS_FFF = "yyyy/MM/dd HH:mm:ss.fff",
 }
 
 const formatterMap = new Map([
-  [FormatType.HH_MM_SS_FFF, logTimeFormatterHhMmSsFff]
+  [FormatType.HH_MM_SS_FFF, logTimeFormatterHhMmSsFff],
+  [FormatType.YYYY_MM_DD_HH_MM_SS_FFF, logTimeFormatterYyyyMMddHhMmSsFff],
 ]);
 
 export function formatTime(dateValue: Date, format: string = FormatType.HH_MM_SS_FFF) {
   const formatName = (() => {
     switch (format) {
-      case FormatType.HH_MM_SS_FFF:
-        return FormatType.HH_MM_SS_FFF;
+      case FormatType.YYYY_MM_DD_HH_MM_SS_FFF:
+        return FormatType.YYYY_MM_DD_HH_MM_SS_FFF;
       default:
         return FormatType.HH_MM_SS_FFF;
     }
