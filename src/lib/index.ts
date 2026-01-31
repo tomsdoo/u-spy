ae5bdd86f7e431e11d030695203882dc8a1b95f6
tomsdoo <toms@live.jp>
2026-02-01 08:54:15 +0900

    export hotStroke from lib

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,24 @@
+import {
+  getRegisteredHotStroke,
+  getRegisteredHotStrokes,
+  registerHotStroke,
+} from "@/key-event";
+
 export { ensureTemplateView } from "@/components/dynamic-element/template-view";
+export const hotStroke = {
+    register: registerHotStroke,
+    get keys() {
+      return getRegisteredHotStrokes();
+    },
+    get(key: string) {
+      return getRegisteredHotStroke(key);
+    },
+    unregister(key: string) {
+      getRegisteredHotStroke(key)?.unregisterHotStroke();
+    },
+    unregisterAll() {
+      for (const key of getRegisteredHotStrokes()) {
+        getRegisteredHotStroke(key)?.unregisterHotStroke();
+      }
+    },
+};
